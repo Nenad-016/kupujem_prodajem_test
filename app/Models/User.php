f02c9a958e911ca8c224ca401085dfbe49044b65
jovanoti <kolejovanovic@gmail.com>
2025-11-21 22:56:03 +0100

    custom middleware and slight user model adjust

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role'
+        'role',
     ];
 
     /**
@@ -44,5 +44,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isUser(): bool
+    {
+        return $this->role === 'user';
     }
 }
